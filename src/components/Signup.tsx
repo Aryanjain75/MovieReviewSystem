79332712d3c1642e46561d1e76a405dc51ecb2f0
ARYAN1JAIN <jainaryanjain30@gmail.com>
2024-12-24 16:12:18 +0530

    final

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
