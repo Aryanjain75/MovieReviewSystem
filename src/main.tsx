0d995f156f8a6b2fa83b01499bd521c19c0b1abb
ARYAN JAIN <142839466+Aryanjain75@users.noreply.github.com>
2024-12-24 17:35:43 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux';
 import store from './services/store';
 import './index.css';
 import { ContextProvider } from './context/UserDetails';
+import { ToastContainer } from 'react-toastify'; // Import ToastContainer
+import 'react-toastify/dist/ReactToastify.css'; // Import Toastify styles
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
@@ -13,9 +15,20 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ContextProvider>
-      <MovieProvider>
-        <App />
-      </MovieProvider>
+        <MovieProvider>
+          <App />
+          <ToastContainer // Add ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+        </MovieProvider>
       </ContextProvider>
     </Provider>
   </React.StrictMode>
